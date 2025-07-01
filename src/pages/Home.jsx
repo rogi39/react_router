2a360874017b271d7658d45e734068c663578a6f
rogi39 <igor93osetia@yandex.ru>
2025-07-01 16:35:14 +0300

    12.3

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,5 +1,13 @@
+import { Link } from "react-router-dom";
+
 function Home() {
-  return <div>Home</div>;
+  return (
+    <div>
+      <Link to={"/"}>Home</Link>
+      <Link to={"about"}>About</Link>
+      <Link to={"cart"}>Cart</Link>
+    </div>
+  );
 }
 
 export default Home;
