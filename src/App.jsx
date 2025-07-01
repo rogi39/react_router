f15a46e290cfc0c626f2207c90ed37e2ab2a2252
rogi39 <igor93osetia@yandex.ru>
2025-07-01 16:30:07 +0300

    12.2

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,40 @@
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Home from "./pages/Home";
+import About from "./pages/About";
+import Cart from "./pages/Cart";
+import Categories from "./pages/Categories";
+import NotFound from "./pages/NotFound";
+import ProductDetails from "./pages/ProductDetails";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Home />,
+  },
+  {
+    path: "about",
+    element: <About />,
+  },
+  {
+    path: "cart",
+    element: <Cart />,
+  },
+  {
+    path: "categories",
+    element: <Categories />,
+  },
+  {
+    path: "*",
+    element: <NotFound />,
+  },
+  {
+    path: "product",
+    element: <ProductDetails />,
+  },
+]);
+
 function App() {
-  return <div></div>;
+  return <RouterProvider router={router} />;
 }
 
 export default App;
