c153a233c1375ae7a7a3d664f014ee597be2eeac
rogi39 <igor93osetia@yandex.ru>
2025-07-03 15:25:07 +0300

    12.12-12.13

--- src/pages/Category.jsx
+++ src/pages/Category.jsx
@@ -1,31 +1,42 @@
-import { useParams } from "react-router-dom";
+import { useParams, useSearchParams } from "react-router-dom";
 import { Link } from "react-router-dom";
 import { products } from "../data/data";
 
 function Category() {
   const { slug } = useParams();
-  const currentCategoryArr = products.filter((product) => product.categoryId.toLowerCase() === slug);
-  console.log(currentCategoryArr);
+  const [sortParams, setSortParams] = useSearchParams();
+  const maxPrice = sortParams.get("price") ? Number(sortParams.get("price")) : Infinity;
+  const currentCategoryArr = products.filter(
+    (product) => product.categoryId.toLowerCase() === slug && product.price <= maxPrice
+  );
+
+  // console.log(maxPrice);
+
+  function handleChange(e) {
+    const value = e.target.value;
+    setSortParams(value ? { price: value, lalal: "qwe" } : {});
+  }
+
   return (
     <>
-      {currentCategoryArr.length ? (
-        <>
-          <h1>Category {slug}</h1>
-          <ul style={{ display: "flex" }}>
-            {currentCategoryArr.map((item) => (
-              <li key={item.id}>
-                <Link to={`/product/${item.id}`}>
-                  {item.name}
-                  {item.price}
-                  <img src={item.img} alt={item.name} style={{ width: "150px" }} />
-                </Link>
-              </li>
-            ))}
-          </ul>
-        </>
-      ) : (
-        <p>404</p>
-      )}
+      <h1>Category {slug}</h1>
+      <div>
+        <label>
+          Max Price
+          <input type="number" onChange={handleChange} value={sortParams.get("price") || ""} />
+        </label>
+      </div>
+      <ul style={{ display: "flex", gap: "10px" }}>
+        {currentCategoryArr.map((item) => (
+          <li key={item.id}>
+            <Link to={`/product/${item.id}`}>
+              <div>{item.name}</div>
+              <p>{item.price}</p>
+              <img src={item.img} alt={item.name} style={{ width: "150px" }} />
+            </Link>
+          </li>
+        ))}
+      </ul>
     </>
   );
 }
